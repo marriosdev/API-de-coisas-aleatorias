300664d7c777cd0e995e517ef77a35636c0610b2
Edmário Oliveira <49793161+edmariooliver@users.noreply.github.com>
2022-10-21 12:22:01 -0300

    Update Api.php

--- app/Routes/Api.php
+++ app/Routes/Api.php
@@ -6,5 +6,5 @@ $router = new HttpRouter();
 
 // Set route
 $router->get("", [App\Controllers\Api\InicioController::class, "index"])->run();
-$router->get("/api/noticias/todas", [App\Controllers\Api\NoticiaController::class, "buscarTodas"])->run();
+$router->get("/api/noticias", [App\Controllers\Api\NoticiaController::class, "buscarTodas"])->run();
 $router->notFound();
